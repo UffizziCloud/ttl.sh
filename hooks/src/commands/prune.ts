6c9e1cbb0d740e05ea69e39161ba63f1f6b80d0a
Adam Vollrath <adam.d.vollrath@gmail.com>
2023-12-15 15:01:16 -0600

    Query registry and prepare to delete.

--- hooks/src/commands/prune.ts
+++ hooks/src/commands/prune.ts
@@ -28,10 +28,11 @@ if (registryURL == null || registryURL == "") {
 }
 
 const client = redis.createClient({url: process.env["REDISCLOUD_URL"]});
-const smembersAsync = promisify(client.smembers).bind(client);
-const sremAsync = promisify(client.srem).bind(client);
-const hgetAsync = promisify(client.hget).bind(client);
-const delAsync = promisify(client.del).bind(client);
+//const smembersAsync = promisify(client.smembers).bind(client);
+const sismemberAsync = promisify(client.sismember).bind(client);
+//const sremAsync = promisify(client.srem).bind(client);
+//const hgetAsync = promisify(client.hget).bind(client);
+//const delAsync = promisify(client.del).bind(client);
 
 const tagRegex = new RegExp("docker/registry/v2/repositories/(.*)/_manifests/tags/(.*)/current/link");
 
@@ -57,7 +58,7 @@ async function main(argv): Promise<any> {
   let jobRunning: boolean = false;
   
   const job = new CronJob({
-    cronTime: "* * * * *",
+    cronTime: "0 * * * *",
     onTick: async () => {
       if (jobRunning) {
         console.log("-----> previous prune job is still running, skipping");
@@ -92,68 +93,61 @@ async function pruneOrphanedTags() {
     .on('error', (err) => {
       return console.error(err.toString());
     })
-    .on('data', (file) => {
-      console.log(file.name);
+    .on('data', async (file) => {
+      //console.log(file.name);
       const match = file.name.match(tagRegex);
       const tag = `${match[1]}:${match[2]}`;
-      console.log(tag);
+      // console.log(tag);
+      const isMember = await sismemberAsync("current.images", tag);
+      //console.log(isMember);
+      if (isMember == 1) {
+        console.log(tag, " is member.");
+      }
+      else if (isMember == 0) {
+        // console.log(tag, " is NOT a member. Deleting!");
+
+        const imageAndTag = tag.split(":");
+        const headers = {
+          "Accept": "application/vnd.docker.distribution.manifest.v2+json",
+        };
+  
+        //console.log(`querying ${tag}`);
+  
+        // Get the manifest from the tag
+        const getOptions = {
+          method: "HEAD",
+          uri: `${registryURL}/v2/${imageAndTag[0]}/manifests/${imageAndTag[1]}`,
+          headers,
+          resolveWithFullResponse: true,
+          simple: false,
+        }
+        const getResponse = await rp(getOptions);
+  
+        if (getResponse.statusCode == 404) {
+          return console.error("HTTP 404 at ", getOptions.uri);
+        }
+  
+        const deleteURI = `${registryURL}/v2/${imageAndTag[0]}/manifests/${getResponse.headers.etag.replace(/"/g,"")}`;
+  
+        console.log(`deleting ${deleteURI}`);
+  
+        // Remove from the registry
+        const options = {
+          method: "DELETE",
+          uri: deleteURI,
+          headers,
+          resolveWithFullResponse: true,
+          simple: false,
+        }
+  
+        // await rp(options);
+      }
+      else
+      {
+	return console.error("unknown value for SISMEMBER ", tag);
+      }
     })
     .on('end', () => {
       return console.log("stream ended.");
     });
-//  for (const image of images) {
-//    try {
-//      const expireAt = await hgetAsync(image, "expires");
-//
-//      if (+expireAt > now) {
-//        const minutesLeft = (+expireAt - now) / 1000 / 60;
-//        console.log(`not expiring ${image} for another ~${Math.round(minutesLeft)} minute(s)`);
-//        continue;
-//      }
-//
-//      const imageAndTag = image.split(":");
-//      const headers = {
-//        "Accept": "application/vnd.docker.distribution.manifest.v2+json",
-//      };
-//
-//      console.log(`querying ${image}`);
-//
-//      // Get the manifest from the tag
-//      const getOptions = {
-//        method: "HEAD",
-//        uri: `${registryURL}/v2/${imageAndTag[0]}/manifests/${imageAndTag[1]}`,
-//        headers,
-//        resolveWithFullResponse: true,
-//        simple: false,
-//      }
-//      const getResponse = await rp(getOptions);
-//
-//      if (getResponse.statusCode == 404) {
-//        await sremAsync("current.images", image);
-//        await delAsync(image);
-//        continue;
-//      }
-//
-//      const deleteURI = `${registryURL}/v2/${imageAndTag[0]}/manifests/${getResponse.headers.etag.replace(/"/g,"")}`;
-//
-//      //console.log(`deleting ${deleteURI}`);
-//
-//      // Remove from the registry
-//      const options = {
-//        method: "DELETE",
-//        uri: deleteURI,
-//        headers,
-//        resolveWithFullResponse: true,
-//        simple: false,
-//      }
-//
-//      await rp(options);
-//
-//      console.log(`expiring ${image}`);
-//      await sremAsync("current.images", image);
-//      await delAsync(image);
-//    } catch(err) {
-//      console.log(`failed to evaluate image ${image}:`, err);
-//    }
-//  }
 }
